7445423f666c8f4b49ac1c7ad16d17ffe7abdadd
Mikhail <miha-chernikov@mail.ru>
2022-03-24 18:09:14 +0300

    Lesson 4. Data from files

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -20,9 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = (
-    "django-insecure-$)esb@et_82*jkk^i$#$i4md#g-d6&juazrq))gd3$1nrv)cll"
-)
+SECRET_KEY = "django-insecure-$)esb@et_82*jkk^i$#$i4md#g-d6&juazrq))gd3$1nrv)cll"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
