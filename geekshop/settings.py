80d43f84e7d0c5186655f1d7c70ba92e52778d8d
Mikhail <miha-chernikov@mail.ru>
2022-04-13 12:34:41 +0300

    lesson_7. Adminapp.

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -33,15 +33,16 @@ ALLOWED_HOSTS = ["*"]
 # Application definition
 
 INSTALLED_APPS = [
-    "django.contrib.admin",
-    "django.contrib.auth",
-    "django.contrib.contenttypes",
-    "django.contrib.sessions",
-    "django.contrib.messages",
-    "django.contrib.staticfiles",
-    "mainapp",
-    "authnapp",
-    "basketapp",
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'mainapp',
+    'authnapp',
+    'basketapp',
+    'adminapp',
 ]
 
 # Auth model
@@ -145,3 +146,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 # Set login path:
 #   https://docs.djangoproject.com/en/2.2/ref/settings/#login-url
 LOGIN_URL = "authnapp:login"
+
