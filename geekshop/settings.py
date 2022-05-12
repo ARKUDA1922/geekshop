5ce7d24772ae6e8f20a4302c1622ca650844a933
Mikhail <miha-chernikov@mail.ru>
2022-05-12 20:01:56 +0300

    lesson_14. Load testing

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -43,7 +43,7 @@ MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
-    "django.middleware.csrf.CsrfViewMiddleware",
+    # "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
@@ -202,17 +202,21 @@ SOCIAL_AUTH_VK_OAUTH2_SECRET = vk_auth["SOCIAL_AUTH_VK_OAUTH2_SECRET"]
 
 
 if DEBUG:
-    INSTALLED_APPS.extend([
-        "debug_toolbar",
-        "template_profiler_panel",
-        "django_extensions",
-    ])
+    INSTALLED_APPS.extend(
+        [
+            "debug_toolbar",
+            "template_profiler_panel",
+            "django_extensions",
+        ]
+    )
 
 
 if DEBUG:
-    MIDDLEWARE.extend([
-        "debug_toolbar.middleware.DebugToolbarMiddleware",
-    ])
+    MIDDLEWARE.extend(
+        [
+            "debug_toolbar.middleware.DebugToolbarMiddleware",
+        ]
+    )
 
 # Debgu tool bar settings
 if DEBUG:
